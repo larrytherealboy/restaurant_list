b96a64783f458f3ecaa07ed3f323cc669eb04bdd
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-17 22:34:05 +0800

    Initialize Project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// require package
 const express = require('express')
 const app = express()
 const port = 3000
+const restaurantList = require('./restaurant.json')
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -14,9 +16,23 @@ app.use(express.static('public'))
 
 // setting roate
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id === Number(req.params.restaurant_id))
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.includes(keyword) || restaurant.category.includes(keyword)
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listening express server
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
