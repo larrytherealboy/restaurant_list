eb6cce30b1ee78a90d70331ecf42a2dfcea3ac89
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-27 17:37:23 +0800

    setting database

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 // require package
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+// const restaurantList = require('./restaurant.json')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
-const restaurantList = require('./restaurant.json')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
